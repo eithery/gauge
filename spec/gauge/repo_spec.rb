51e20f85a0ef2f68fc6427cbe29fca50b654d24a
Michael Protsenko <michael.protsenko@gmail.com>
2014-10-29 23:26:59 -0400

    Created spec for Repo class.

---
+++ spec/gauge/repo_spec.rb
@@ -0,0 +1,14 @@
+# Eithery Lab., 2014.
+# Gauge::Repo specs.
+require 'spec_helper'
+
+module Gauge
+  describe Repo do
+    describe '#initialize' do
+    end
+
+
+    describe '#validate' do
+    end
+  end
+end
