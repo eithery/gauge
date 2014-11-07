ca9b3c516bdddf6abd823edb51af0ecc7db6b3ad
Michael Protsenko <michael.protsenko@gmail.com>
2014-11-07 18:25:01 -0500

    Defined databases.rb file as metadata file for all available databases.

---
+++ config/databases.rb
@@ -0,0 +1,13 @@
+# Database definition file.
+# Each 'database' declaration defines metadata for one particular database.
+#
+# Example:
+#   database <database_name>, sql_name: <sql_database_name>
+#
+#   <database_name> - logical database name used in most command-line operations;
+#   :sql_name       - represents physical SQL database name (the same as logical database name by default).
+#
+# Data tables metadata files can be found in db/<database_name> folder and all its subfolders.
+
+database :rep_profile, sql_name: 'RepProfile'
+database :package_me, sql_name: 'PackageMe'
--- db/databases.rb
+++
@@ -1,5 +0,0 @@
-# Database definition file.
-# Contains metadata for databases.
-
-database :rep_profile, db_name: 'RepProfile'
-database :package_me, db_name: 'PackageMe'
