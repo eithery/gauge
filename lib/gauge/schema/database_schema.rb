bf570aae6f97b0d8e18146bb13a75648cb2eae45
Michael Protsenko <michael.protsenko@gmail.com>
2014-11-07 23:12:51 -0500

    Modified DatabaseSchema class to support MetadataFactory.

--- lib/gauge/schema/database_schema.rb
+++ lib/gauge/schema/database_schema.rb
@@ -9,15 +9,14 @@ module Gauge
 		class DatabaseSchema
 			attr_reader :database_name, :tables
 
-			def initialize(database_name, options)
-#				db_definition_file = File.join(data_root, 'databases.rb')
-#				raise "Metadata for '#{database}' database is not defined." unless File.exists?(db_definition_file)
-
-#				require db_definition_file
-
+			def initialize(database_name, options={})
 				@database_name = database_name
+				@options = options
 				@tables = {}
 
+#				db_definition_file = File.join(data_root, 'databases.rb')
+#				raise "Metadata for '#{database}' database is not defined." unless File.exists?(db_definition_file)
+#				require db_definition_file
 #				Dir["#{data_root}/#{database_name}/**/*.db.xml"].map do |schema_file|
 #					table_schema = DataTableSchema.new(schema_file)
 #					@tables[table_schema.to_key] = table_schema
