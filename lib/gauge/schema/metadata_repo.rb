cd603e0d3a5c00fd4da7247638a7675ad63f9df1
Michael Protsenko <michael.protsenko@gmail.com>
2014-11-07 22:25:21 -0500

    Created MetadataRepo class representing database metadata repository.

---
+++ lib/gauge/schema/metadata_repo.rb
@@ -0,0 +1,11 @@
+# Eithery Lab., 2014.
+# Class Gauge::Schema::MetadataRepo
+# Represent a repository storing metadata describing database structure.
+require 'gauge'
+
+module Gauge
+  module Schema
+    class MetadataRepo
+    end
+  end
+end
---
+++ spec/unit/gauge/schema/metadata_repo_spec.rb
@@ -0,0 +1,10 @@
+# Eithery Lab., 2014.
+# Gauge::Schema::MetadataRepo specs.
+require 'spec_helper'
+
+module Gauge
+  module Schema
+    describe MetadataRepo do
+    end
+  end
+end
