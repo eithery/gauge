a2389c5cdec96419f13407829b69b0343dd796fc
Michael Protsenko <michael.protsenko@gmail.com>
2014-11-21 18:45:49 -0500

    Created ColumnLengthValidator class verifying the length of the data column.

---
+++ lib/gauge/validators/column_length_validator.rb
@@ -0,0 +1,30 @@
+# Eithery Lab., 2014.
+# Class Gauge::Validators::ColumnLengthValidator
+# Checks length for string type data columns.
+require 'gauge'
+
+module Gauge
+  module Validators
+    class ColumnLengthValidator < Validators::Base
+
+      validate do |column_schema, db_column|
+        len = db_column[:max_chars]
+
+        if length_mismatch? column_schema, len
+
+          errors << "The length of data column '".color(:red) +  column_schema.column_name.to_s.color(:red).bright +
+            "' is '".color(:red) + "#{len}".color(:red).bright + "', but it must be '".color(:red) +
+            "#{column_schema.length}".color(:red).bright + "' symbols.".color(:red)
+        end
+      end
+
+  private
+
+      def length_mismatch?(column_schema, length)
+        return false unless column_schema.column_type == :string
+        return false if length == -1 && column_schema.length == :max
+        column_schema.length != length
+      end
+    end
+  end
+end
