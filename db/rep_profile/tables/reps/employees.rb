f430e6fa6e7eef0c00a499350a866329aeef25cf
Michael Protsenko <michael.protsenko@gmail.com>
2014-11-23 15:30:45 -0500

    Fixed incorrect default constraints in [employee] data table definition.

--- db/rep_profile/tables/reps/employees.rb
+++ db/rep_profile/tables/reps/employees.rb
@@ -11,12 +11,12 @@ table :employees do
   col :displayName
   col :ssn, len: 16
   col :employeeCode, len: 16
-  col :gender, type: :char, check: %(F M)
+  col :gender, type: :char, len: 1, check: %(F M)
   col :dateOfBirth, type: :datetime
   col :businessAddressId, :ref => :addresses
   col :residenceAddressId, :ref => :addresses
   col :residenceAddress2Id, :ref => :addresses
-  col :preferredMailingAddress, type: :enum, required: true, check: 0..3
+  col :preferredMailingAddress, type: :enum, required: true, default: 0, check: 0..3
   col :contactsId, :ref => :contacts
   col :description, len: :max
   col :photo, type: :blob
