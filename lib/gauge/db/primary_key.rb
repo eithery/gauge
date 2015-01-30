602028012fb766dcfcf9ded8bdda679151e76796
Michael Protsenko <michael.protsenko@gmail.com>
2015-01-29 23:22:19 -0500

    Designed PrimaryKey class representing primary key constraint. Implemented spec prototypes for PrimaryKey class.

--- lib/gauge/db/primary_key.rb
+++ lib/gauge/db/primary_key.rb
@@ -1,5 +1,5 @@
 # Eithery Lab., 2015.
-# Class Gauge::DB::PrimaryKeyConstraint
+# Class Gauge::DB::PrimaryKey
 # Represents the actual primary key constraint.
 
 require 'gauge'
@@ -7,6 +7,29 @@ require 'gauge'
 module Gauge
   module DB
     class PrimaryKey
+      attr_reader :name, :table, :columns
+
+      def initialize(name, table, columns, options={})
+        @name = name
+        @table = table
+        @columns = columns
+        @clustered = clustered
+      end
+
+
+      def columns
+        @columns ||= []
+      end
+
+
+      def clustered?
+        @clustered
+      end
+
+
+      def composite?
+        columns.length > 1
+      end
     end
   end
 end
--- spec/unit/gauge/db/primary_key_spec.rb
+++ spec/unit/gauge/db/primary_key_spec.rb
@@ -6,6 +6,43 @@ require 'spec_helper'
 module Gauge
   module DB
     describe PrimaryKey do
+      let(:key) { PrimaryKey.new('pk_primary_reps', 'br.primary_reps', :rep_code) }
+      subject { key }
+
+      it { should respond_to :name }
+      it { should respond_to :table }
+      it { should respond_to :columns }
+      it { should respond_to :clustered?, :composite? }
+
+
+      describe '#name' do
+        it "equals to the key name passed in the initializer" do
+          key.name.should == 'pk_primary_reps'
+        end
+      end
+
+
+      describe '#table' do
+        it "equals to the table name passed in the initializer" do
+          key.table.should == 'br.primary_reps'
+        end
+      end
+
+
+      describe '#columns' do
+        specify { key.columns.should include(:rep_code) }
+      end
+
+
+      describe '#clustered?' do
+        context "when not defined explicitly in the initializer" do
+          it { should be true }
+        end
+      end
+
+
+      describe '#composite?' do
+      end
     end
   end
 end
