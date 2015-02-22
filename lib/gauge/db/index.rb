3b8058d5676fffe857aafd0d140481adf03a4cdc
Michael Protsenko <michael.protsenko@gmail.com>
2015-02-22 18:38:08 -0500

    Created Index class encapsulating database index functionality.

---
+++ lib/gauge/db/index.rb
@@ -0,0 +1,10 @@
+# Eithery Lab., 2015.
+# Class Gauge::DB::Index
+# Represents the database index on the data table or data view.
+
+module Gauge
+  module DB
+    class Index
+    end
+  end
+end
---
+++ spec/unit/gauge/db/index_spec.rb
@@ -0,0 +1,11 @@
+# Eithery Lab., 2015.
+# Class Gauge::DB::Index specs.
+
+require 'spec_helper'
+
+module Gauge
+  module DB
+    describe Index do
+    end
+  end
+end
