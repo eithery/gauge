39683282e64b32232fbc09b172f6545ceb0ecf03
Michael Protsenko <michael.protsenko@gmail.com>
2015-02-23 00:36:43 -0500

    Implemented DataTable class and its spec definition.

--- lib/gauge/db/data_table.rb
+++ lib/gauge/db/data_table.rb
@@ -6,7 +6,34 @@ require 'gauge'
 
 module Gauge
   module DB
-    class DataTable
+    class DataTable < DatabaseObject
+      def initialize(name)
+        super
+      end
+
+
+      def columns
+      end
+
+
+      def primary_key
+      end
+
+
+      def foreign_keys
+      end
+
+
+      def unique_constraints
+      end
+
+
+      def check_constraints
+      end
+
+
+      def indexes
+      end
     end
   end
 end
--- spec/unit/gauge/db/data_table_spec.rb
+++ spec/unit/gauge/db/data_table_spec.rb
@@ -6,6 +6,42 @@ require 'spec_helper'
 module Gauge
   module DB
     describe DataTable do
+      let(:dbo_name) { 'PRIMARY_REPS' }
+      let(:dbo) { DataTable.new(dbo_name) }
+      subject { dbo }
+
+      it_behaves_like "any database object"
+
+      it { should respond_to :columns }
+      it { should respond_to :primary_key }
+      it { should respond_to :foreign_keys }
+      it { should respond_to :unique_constraints }
+      it { should respond_to :check_constraints }
+      it { should respond_to :indexes }
+
+
+      describe '#columns' do
+      end
+
+
+      describe '#primary_key' do
+      end
+
+
+      describe '#foreign_keys' do
+      end
+
+
+      describe '#unique_constraints' do
+      end
+
+
+      describe '#check_constraints' do
+      end
+
+
+      describe '#indexes' do
+      end
     end
   end
 end
