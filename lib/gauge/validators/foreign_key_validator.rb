5b362e8b4323fa2863346dc9690d764607c47c3f
Michael Protsenko <michael.protsenko@gmail.com>
2015-04-22 12:49:47 -0400

    Implemented behavior of ForeignKeyValidator class performing validation check for missing and mismatched foreign keys.

--- lib/gauge/validators/foreign_key_validator.rb
+++ lib/gauge/validators/foreign_key_validator.rb
@@ -9,6 +9,58 @@ module Gauge
     class ForeignKeyValidator < Validators::Base
 
       validate do |table_schema, table, sql|
+        table_schema.foreign_keys.each do |expected_key|
+          actual_key = foreign_key_on(table, expected_key.columns)
+          case mismatch(expected_key, actual_key)
+            when :missing_foreign_key
+              errors << "Missing #{description_of(expected_key)}"
+            when :ref_table_mismatch
+              errors << ref_table_mismatch_message(expected_key, actual_key)
+            when :ref_columns_mismatch
+              errors << ref_columns_mismatch_message(expected_key, actual_key)
+          end
+        end
+      end
+
+  private
+
+      def mismatch(expected, actual)
+        return :missing_foreign_key if actual.nil?
+        return :ref_table_mismatch unless actual.ref_table == expected.ref_table
+        return :ref_columns_mismatch unless actual.ref_columns.sort == expected.ref_columns.sort
+      end
+
+
+      def description_of(foreign_key)
+        "<b>foreign key</b> on [#{displayed_names_of(foreign_key.columns)}] " +
+        "column".pluralize(foreign_key.columns.count) +
+        " references to ['<b>#{foreign_key.ref_table}</b>'].[#{displayed_names_of(foreign_key.ref_columns)}]."
+      end
+
+
+      def foreign_key_on(table, columns)
+        table.foreign_keys.select { |fk| fk.columns.sort == columns.sort }.first
+      end
+
+
+      def displayed_names_of(columns)
+        columns.map { |col| "'<b>#{col}</b>'" }.join(', ')
+      end
+
+
+      def ref_table_mismatch_message(expected, actual)
+        "<b>Foreign key</b> on [#{displayed_names_of(actual.columns)}] " +
+        "column".pluralize(actual.columns.count) + " references to ['<b>#{actual.ref_table}</b>'] data table, " +
+        "but should be to ['<b>#{expected.ref_table}</b>'] one."
+      end
+
+
+      def ref_columns_mismatch_message(expected, actual)
+        "<b>Foreign key</b> on [#{displayed_names_of(actual.columns)}] " +
+        "column".pluralize(actual.columns.count) +
+        " references to [#{displayed_names_of(actual.ref_columns)}] " +
+        "column".pluralize(actual.ref_columns.count) +
+        ", but should be to [#{displayed_names_of(expected.ref_columns)}]."
       end
     end
   end
--- spec/unit/gauge/validators/foreign_key_validator_spec.rb
+++ spec/unit/gauge/validators/foreign_key_validator_spec.rb
@@ -7,12 +7,140 @@ module Gauge
   module Validators
     describe ForeignKeyValidator do
       let(:validator) { ForeignKeyValidator.new }
+      let(:schema) { @table_schema }
+      let(:table) { double('table', foreign_keys: @foreign_keys) }
+      let(:sql) { SQL::Builder.new }
 
       it { should respond_to :do_validate }
       it_behaves_like "any database object validator"
 
 
       describe '#validate' do
+        context "a simple (one column) foreign key" do
+          before do
+            @table_schema = Gauge::Schema::DataTableSchema.new(:accounts) do
+              col :number, len: 20
+              col :ref => 'bnr.reps', required: true
+            end
+          end
+
+          context "when the same actual foreign key defined" do
+            before do
+              @foreign_keys = [Gauge::DB::Constraints::ForeignKeyConstraint.new('fk_dbo_accounts_rep_id',
+                :accounts, :rep_id, 'bnr.reps', :id)]
+            end
+
+            it { should_not_yield_errors }
+            it { is_expected_not_to_generate_sql }
+          end
+
+          context "when actual foreign key has a different name" do
+            before do
+              @foreign_keys = [Gauge::DB::Constraints::ForeignKeyConstraint.new('fk_dbo_accounts_12345',
+                :accounts, :rep_id, 'bnr.Reps', :id)]
+            end
+
+            it { should_not_yield_errors }
+            it { is_expected_not_to_generate_sql }
+          end
+
+          context "when missing actual foreign key" do
+            before { @foreign_keys = [] }
+            it { yields_error :missing_foreign_key, columns: [:rep_id], ref_table: :bnr_reps, ref_columns: [:id] }
+          end
+
+          context "when actual foreign key defined on another column" do
+            before do
+              @foreign_keys= [Gauge::DB::Constraints::ForeignKeyConstraint.new('fk_dbo_accounts_rep_id',
+                :accounts, :office_id, 'bnr.reps', :id)]
+            end
+
+            it { yields_error :missing_foreign_key, columns: [:rep_id], ref_table: :bnr_reps, ref_columns: [:id] }
+          end
+
+          context "when actual foreign key references to another table" do
+            before do
+              @foreign_keys = [Gauge::DB::Constraints::ForeignKeyConstraint.new('fk_dbo_accounts_rep_id',
+                :accounts, :rep_id, :reps, :id)]
+            end
+
+            it do
+              yields_error :ref_table_mismatch, columns: [:rep_id],
+                ref_table: :bnr_reps, actual_ref_table: :dbo_reps
+            end
+          end
+
+          context "when actual foreign key references to another column" do
+            before do
+              @foreign_keys = [Gauge::DB::Constraints::ForeignKeyConstraint.new('fk_dbo_accounts_rep_id',
+                :accounts, :rep_id, 'bnr.reps', :rep_id)]
+            end
+
+            it do
+              yields_error :ref_columns_mismatch, columns: [:rep_id],
+                ref_columns: [:id], actual_ref_columns: [:rep_id]
+            end
+          end
+        end
+
+        context "for composite foreign key" do
+        end
+      end
+
+  private
+
+      def dba
+        table
+      end
+
+
+      def validate
+        validator.do_validate schema, dba, sql
+      end
+
+
+      def is_expected_not_to_generate_sql
+        sql.should_receive(:drop_constraint).never
+        sql.should_receive(:add_foreign_key).never
+        validate
+      end
+
+
+      def missing_foreign_key_message(options)
+        /Missing #{foreign_key_description(options)}/
+      end
+
+
+      def ref_table_mismatch_message(options)
+        columns = options[:columns]
+        ref_table = options[:ref_table]
+        actual_ref_table = options[:actual_ref_table]
+        message = "(.*?)Foreign key(.*?) on \\[#{displayed_names_of(columns)}\\] " +
+          "column".pluralize(columns.count) + " references to \\['(.*?)#{actual_ref_table}(.*?)'\\] data table, " +
+          "but should be to \\['(.*?)#{ref_table}(.*?)'\\] one."
+        /#{message}/
+      end
+
+
+      def ref_columns_mismatch_message(options)
+        columns = options[:columns]
+        ref_columns = options[:ref_columns]
+        actual_ref_columns = options[:actual_ref_columns]
+        message = "(.*?)Foreign key(.*?) on \\[#{displayed_names_of(columns)}\\] " +
+          "column".pluralize(columns.count) + " references to \\[#{displayed_names_of(actual_ref_columns)}\\] " +
+          "column".pluralize(actual_ref_columns.count) +
+          ", but should be to \\[#{displayed_names_of(ref_columns)}\\]."
+        /#{message}/
+      end
+
+
+      def foreign_key_description(options)
+        columns = options[:columns]
+        ref_table = options[:ref_table]
+        ref_columns = options[:ref_columns]
+        "(.*?)foreign key(.*?) on \\[#{displayed_names_of(columns)}\\] " +
+        "column".pluralize(columns.count) + " references to \\['(.*?)#{ref_table}(.*?)'\\]\\." +
+        "\\[#{displayed_names_of(ref_columns)}\\]"
       end
     end
   end
